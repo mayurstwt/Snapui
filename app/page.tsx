8ffe6fa131ebb3605260b9ec4da5706c151b5195
mayurstwt <mayurstwt@.com>
2025-04-10 22:57:57 +0530

    bugs solved

--- app/page.tsx
+++ app/page.tsx
@@ -11,17 +11,17 @@ import SimpleNavbarTwo from "./Navbars/SimpleNavbarTwo/page";
 
 export default function Home() {
   return (<>
-    <div className="flex flex-col gap-y-40">
-    {/* <SimpleNavbarFour/> */}
-    {/* <SimpleNavbarThree/> */}
-    {/* <SimpleNavbarTwo/> */}
-    {/* <SimpleNavbarOne /> */}
-    {/* <SimpleNavbarFive/> */}
+    <div className="flex flex-col gap-[100vh]">
+    <SimpleNavbarFour/>
+    <SimpleNavbarThree/>
+    <SimpleNavbarTwo/>
+    <SimpleNavbarOne />
+    <SimpleNavbarFive/>
     <SimpleNavbarSix/>
-    {/* <SimpleNavbarSeven/> */}
-    {/* <SimpleNavbarEight/> */}
-    {/* <SimpleNavbarNine/> */}
-    {/* <SimpleNavbarTen/> */}
+    <SimpleNavbarSeven/>
+    <SimpleNavbarEight/>
+    <SimpleNavbarNine/>
+    <SimpleNavbarTen/>
     </div>
   </>)
 }
