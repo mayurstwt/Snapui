65075e756858a09d515a481c6cde962eebf6bb48
mayurstwt <mayurstwt@.com>
2025-04-16 22:06:54 +0530

    files added

--- app/Navbars/SimpleNavbarFour/page.tsx
+++ app/Navbars/SimpleNavbarFour/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useState, useEffect, useRef } from "react";
-import { cn } from "@d/lib/utils";
+import { cn } from "@/lib/utils";
 import { Menu, X, MoonStar, Sun, Github, Twitter, Code } from "lucide-react";
 
 const navLinks = [
